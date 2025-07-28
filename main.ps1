69cfcaa2b35dae7a7af3755d9b68b6b12cf3844a
Sreekar Ramisetty <hindi6476@gmail.com>
2025-07-27 23:29:34 -0500

    added some more reg tweaks (i love regedit)

--- main.ps1
+++ main.ps1
@@ -12,20 +12,22 @@ winget install -e --id Mojang.MinecraftLauncher
 # various config
 OOSU10.exe OOSU10.cfg /nosrp
 netsh interface ipv4 set dnsservers name="Wi-Fi" static 1.1.1.1 index=1 # what happened to index 0 lmao
-netsh interface ipv4 set dnsservers name="Wi-Fi" static 1.1.1.1 index=2
+netsh interface ipv4 set dnsservers name="Wi-Fi" static 1.0.0.1 index=2
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "HideFileExt" -Value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced" -Name "Hidden" -Value 1
 New-ItemProperty -Path "HKLM:\SYSTEM\CurrentControlSet\Control\FileSystem" -Name "LongPathsEnabled" -Value 1 -PropertyType DWORD -Force
 
 Write-Host "Disabling mouse acceleration..."
-Set-ItemProperty -Path "HKCU:\Control Panel\Mouse\MouseSpeed" -Value 0
-Set-ItemProperty -Path "HKCU:\Control Panel\Mouse\MouseThreshold1" -Value 0
-Set-ItemProperty -Path "HKCU:\Control Panel\Mouse\MouseThreshold2" -Value 0
+Set-ItemProperty -Path "HKCU:\Control Panel\Mouse\" -Name "MouseSpeed" -Value 0
+Set-ItemProperty -Path "HKCU:\Control Panel\Mouse\" -Name "MouseThreshold1" -Value 0
+Set-ItemProperty -Path "HKCU:\Control Panel\Mouse\" -Name "MouseThreshold2" -Value 0
 
 Write-Host "Enabling Dark Mode..."
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "AppsUseLightTheme" -Value 0
 Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Themes\Personalize" -Name "SystemUsesLightTheme" -Value 0
-Set-ItemProperty -Path "HKCU\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarDeveloperSettings\" -Name "TaskbarEndTask" -Value 1 # i opened up a whole ahh procmon for ts bruh regedit has not that good documentation
+Set-ItemProperty -Path "HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced\TaskbarDeveloperSettings" -Name "TaskbarEndTask" -Value 1 # i opened up a whole ahh procmon for ts but it's worth it
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Windows\CurrentVersion\AppModelUnlock" /t REG_DWORD /f /v "AllowDevelopmentWithoutDevLicense" /d "1"
+sudo config --enable enable
 
 Write-Host "Editing PATH..."
 $path = [System.Environment]::GetEnvironmentVariable('Path', 'Machine')
@@ -34,7 +36,8 @@ $newPath = "$path;C:\Users\Public\wpilib\2025\vscode\bin"
 
 Write-Host "Downloading WPILib..."
 $response = Invoke-RestMethod "https://api.github.com/repos/wpilibsuite/allwpilib/releases/latest"
-if ($response.body -match "(https[^\s`"']*WPILib_Windows[^\s`"'\)]*)") {
+$body = $response.body
+if ($body -match "(https[^\s`"']*WPILib_Windows[^\s`"'\)]*)") {
     $url = $matches[1]
     Write-Output $url
 
@@ -47,6 +50,8 @@ mkdir wpilib # call me linux
 Copy-Item -Path "$isodrive\*" -Destination "./wpilib" -Recurse -Force
 ./wpilib/wpilib.exe
 # configure your own installation bozo
+Write-Host "Installing Linux..."
+wsl --install # ts so Wait... This isn't what I typed! now
 Start-Process "https://imgur.com/a/name-4k-wallpaper-galore-ShAOh" # 50-50 they want to use ts wallpapers so leave it there
 Start-Process "https://www.youtube.com/watch?v=dQw4w9WgXcQ"
 Start-Process "https://github.com/Sreekar617/setup-ducky"
